4abefaf64bd493518b26d21a9e27d0ed92f2d0df
P5Chydl0 <paul.schydlo@tecnico.ulisboa.pt>
2016-08-08 18:47:05 +0100

    Adds Kt and Ra. Moves ploting to outside of function

--- scripts/MotorSSsim.m
+++ scripts/MotorSSsim.m
@@ -1,4 +1,4 @@
-function [] = MotorSSsim(u,u_nominal,w_no_load,torque_stall)
+function [ v_sim, T_sim, I_sim ] = MotorSSsim(u,u_nominal,w_no_load,torque_stall)
 %% Simulation parameters
 
 u_start = u(1);
@@ -9,12 +9,12 @@ u_step  = u(2);
 
 g  = 9.81;
 
-%Torques resistentes
+% Torques resistentes
 cw = 0.002; % Wheel friction
 cd = 0.18;  % Air drag
 teta = 0;   % Ground slope
 
-%Parâmetros fisicos do carro
+% Parâmetros fisicos do carro
 r = 4 * 10^-2; % Wheel radius
 
 wc = 0.025;   % Chasi weight
@@ -24,9 +24,15 @@ wb = 6*0.030; % 6 AA Battery's weight
 
 w = (wc + wm + wa + wb);  % Total weight
  
-% Motor parameters
-c1 = w_no_load/u_nominal;
+%%  Motor parameters
+
+c1 = w_no_load/u_nominal;          % Parameters of torque curve 
 c2 = (c1*u_nominal)/torque_stall;
+
+kt = 1/c1;
+Ra = c2*kt^2;
+
+fprintf('Kt: %f \nRa: %f\n\n', kt, Ra);
 
 %% Symbolic variables
 
@@ -47,28 +53,31 @@ F(2) = -v/r + c1*u - c2*T;      % Motor equation in steady state
 
 f = solve(F(1),F(2),v,T);  % Solve equations as a function of the voltage
 
-fv = f.v(1);                 % Velocity as function of voltage
-ft = f.T(1);                 % Torque as function of voltage
+fv = f.v(1);               % Velocity as function of voltage
+ft = f.T(1);               % Torque as function of voltage
 
 %% Plot results
 
 u_range = u_start:u_step:u_end;
-v_res   = zeros(length(u_range), 1);
+
+v_sim = zeros(length(u_range), 1);
+T_sim = zeros(length(u_range), 1);
 
 for u_value = u_range
     index = int16((u_value-u_start)/u_step + 1);
    
     v_value = double(subs(fv, u, u_value)); % Substitute the voltage in the function
-    if ( v_value < 0); v_value = 0; end;    % Negative velocities are not covered in this model 
+    t_value = double(subs(ft, u, u_value));
     
-    v_res(index) = v_value;
+    if ( v_value < 0); v_value = 0; t_value = 0; end;    % Negative velocities are not covered in this model 
+    
+    v_sim(index) = v_value;
+    T_sim(index) = t_value;
 end
 
-%Resposta da velocidade
-plot(u_range, v_res)
-title('Velocidade');
-xlabel('Ua [V]');
-ylabel('v [m/s]');
+I_sim = T_sim/kt;
+
+% Resposta da velocidade
 
 end
 
--- scripts/MotorTests.m
+++ scripts/MotorTests.m
@@ -1,5 +1,22 @@
-%Motor 1
-MotorSSsim([2,0.1,6.5],6,rpm2rad(12530),0.0340);
-%Motor 2
+figure;
+title('Velocidade');
+xlabel('Ua [V]');
+ylabel('v [m/s]');
 hold on;
-MotorSSsim([2,0.1,6.5],4.5,rpm2rad(80),210e-3);
+
+u = [2,0.1,6.5];
+u_range = u(1):u(2):u(3);
+
+%Motor 1
+[v, T, I] = MotorSSsim(u ,6,rpm2rad(14500),340e-5);
+
+plot(u_range, v);
+plot(u_range, T,'.');
+plot(u_range, I,'.');
+
+%Motor 2
+[v, T, I] = MotorSSsim(u ,4.5,rpm2rad(80),210e-3);
+
+plot(u_range, v);
+plot(u_range, T,'.');
+plot(u_range, I,'.');
