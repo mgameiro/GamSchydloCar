1c12bdff2d5e385351b48dbfb62b22bb68941758
Miguel Gameiro <gamador@sapo.pt>
2016-08-13 15:28:36 +0100

    Criação do projecto para escrita de código para arduino.

---
+++ arduino/EleCar.ino
@@ -0,0 +1,30 @@
+double calculateSpeed(double Ua, double Ia){
+  // Calculate the car speed from voltage and current
+  double w;
+  
+  w = (Ua-Ra*Ia)/Kf;
+  v = w*R;
+  return v;
+}
+
+double readOutputVoltage(){
+  // read the value of voltage at output
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+
+  // DC Motor constants
+  const double Ra = 7.5;
+  const double Kf = 1.1234;
+  const double Kt = 0.8827;
+  // Car physical constants
+  const double M = 0.5;
+  const double R = 0.003;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
