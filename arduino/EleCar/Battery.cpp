bc10ec5ccd2ef84f3c906969de68de16c5b4e413
mgameiro <miguelgameiro94@hotmail.com>
2016-11-22 21:19:08 +0000

    Retirado código excessivamente pedantico

--- arduino/EleCar/Battery.cpp
+++ arduino/EleCar/Battery.cpp
@@ -8,7 +8,7 @@ bool Battery::isBatteryCharged(){
   // Verify battery state (charged or not)
   bool charged;
   double ub;
-  ub = this->ai.readinputPinVoltage(PINBATTERY);
+  ub = (5.0/1024.0)*analogRead(PINBATTERY);
   if(ub >= BATTERYMINCHARGED){
     charged = true;
   } else {
