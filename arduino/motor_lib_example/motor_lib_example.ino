6121d68878320de11e6898044e2c4e36af91034b
P5Chydl0 <paul.schydlo@tecnico.ulisboa.pt>
2017-01-08 17:26:32 +0000

    Adds coments to motor example

--- arduino/motor_lib_example/motor_lib_example.ino
+++ arduino/motor_lib_example/motor_lib_example.ino
@@ -9,32 +9,42 @@ AF_DCMotor motor4(1, MOTOR12_64KHZ);
 int trigPin = 14;
 int echoPin = 15;
 
-long distanz;
-long distanz_cm;
+long distance_cm;
 
 NewPing sonar(trigPin, echoPin, max_cm_distance);
 
 void setup(){
-  
   motor3.setSpeed(500);
   motor4.setSpeed(500);
 }
 
+void reset_ultrasound(int echoPin){
+  delay(100);
+  pinMode(echoPin, OUTPUT);
+  digitalWrite(echoPin, LOW);
+  delay(100);
+  pinMode(echoPin, INPUT);
+}
+
 void loop(){
   delay(1000);
- 
-  distanz_cm = sonar.ping_cm();
- 
-  if ( distanz_cm == 0 ) {
-      delay(100);
-      pinMode(echoPin, OUTPUT);
-      digitalWrite(echoPin, LOW);
-      delay(100);
-      pinMode(echoPin, INPUT);
 
+  /* Send trigger wave to measure distance */
+  distance_cm = sonar.ping_cm();
+
+  /* Ultrasound stuck or obstacle beyound distance threshold */
+  if ( distance_cm == 0 ) {
+    
+      /* Reset ultrasound to make sure it's not stuck */
+      reset_ultrasound(echoPin);      
+
+      /* Stop motor */
       motor3.run(RELEASE);
       motor4.run(RELEASE);
+  
   }else{
+
+      /* Set motor to forward mode */
       motor3.run(FORWARD);
       motor4.run(FORWARD);
   }
