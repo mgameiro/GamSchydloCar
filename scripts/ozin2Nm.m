131e9f531fc396e9fa591ce1f0877e2827173e31
Unknown <miguelgameiro94@hotmail.com>
2017-04-15 22:27:25 +0100

    Apagados scripts anteriores

--- scripts/ozin2Nm.m
+++
@@ -1,4 +0,0 @@
-function [TNm] = ozin2Nm(Toz)
-   TNm = Toz/141.611932278;    
-end
-
--- scripts/rpm2rad.m
+++
@@ -1,6 +0,0 @@
-function [thetaR] = rpm2rad(thetaD)
-    %Converte velocidades em rpm para rad/s
-    
-    thetaR = (2*pi/60)*thetaD;
-end
-
