135b89ebbf53c13a2d479b48d4f7c88aeffbf16d
mgameiro <miguelgameiro94@hotmail.com>
2017-08-16 22:52:19 +0100

    Improved Sensor class
    - Measured Voltage and Current from the motor
    - Class described

--- arduino/EleCar/Sensor.cpp
+++ arduino/EleCar/Sensor.cpp
@@ -4,7 +4,21 @@ Sensor::Sensor(){
   
 }
 
+double Sensor::getMotorVoltage(){
+  return this->measureAnalogPin(VOLTAGESENSORPIN);
+}
+
+double Sensor::getMotorCurrent(){
+  return this->measureAnalogPin(CURRENTSENSORPIN);
+}
+
 Sensor::~Sensor(){
   
+}
+
+double Sensor::measureAnalogPin(int apin){
+  double apin;
+  rpin = (5.0/1023.0)*analogRead(apin);
+  return rpin;
 }
 
--- arduino/EleCar/Sensor.h
+++ arduino/EleCar/Sensor.h
@@ -1,11 +1,29 @@
 #ifndef SENSOR_H_INCLUDED
 #define SENSOR_H_INCLUDED
 
+//---------------------------------------------------------------
+//    Class: Sensor
+//
+//      This class defines parameters and behaviour of all sensors
+//      coupled with the arduino, as well as all pre-processing
+//      which is necessary for measuring the signals.
+//
+//      Sensors used: voltage, current
+//---------------------------------------------------------------
+
+#define VOLTAGESENSORPIN 1
+#define CURRENTSENSORPIN 2
+
 class Sensor{
 
   public:
     Sensor();
+    double getMotorVoltage();
+    double getMotorCurrent();
     ~Sensor();
+  
+  private:
+    double measureAnalogPin(int apin);
 };
 
 
